65b21cc588e308d65c6d2d60b98b8ce96ee2452d
noorTom <noor.tomok@gmail.com>
2018-11-01 08:52:11 +0400

    3 new commit for week 4

--- Python/Week4-2.py
+++ Python/Week4-2.py
@@ -1,6 +1,6 @@
 print('Numbers from 1 to 10')
 num = int (input ('Guess the number: '))
-if num >10 and num <1 :
+if num >10 or num <1 :
     print('Guess the Number: ')
 else:
-    print ('Great! You did it')    
+    print ('Great! You did it')
