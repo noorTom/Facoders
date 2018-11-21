56ecd87e7893cd68f4ccc288758d4b946ebeb061
noorTom <noor.tomok@gmail.com>
2018-11-21 17:49:48 +0400

    new commit for week 7

---
+++ Python/exr1.py
@@ -0,0 +1,14 @@
+def my_function(a) :
+    x=len(a)
+    i=x-1
+    y=a[0]
+    h=a[i]
+    return y,h
+a=[3,5,6,2,58,9]
+print(my_function(a))
+
+b= [7,8,9,5,2,14,23,65,8]
+print (my_function(b))
+
+c= ['salem','ali','sami','ahmad']
+print(my_function(c))
